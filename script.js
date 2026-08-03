b05febca203c32f528450a62d2e5b64a8c42ed53
Diego <diegofonsca.25@gmail.com>
2026-08-03 20:24:24 -0300

    alterações

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ console.log(menuBtn);
 console.log(navbar);
 
 menuBtn.addEventListener("click", () => {
-  console.log("Cliquei!");
   navbar.classList.toggle("active");
+});
+
+// ANIMAÇÕES FADE IN
+
+const elementosFade = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+elementosFade.forEach((elemento) => {
+  observer.observe(elemento);
 });
